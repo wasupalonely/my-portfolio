60dc77a4d16cef31614dd03284098576846fe256
wasupalonely <juandiegomaciasoy@gmail.com>
2024-05-27 16:59:53 -0500

    First deploy readt

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,40 +1,45 @@
 export interface Project {
-    slug: string;
-    title: string;
-    description: string;
-    imageUrl: string;
-    details: string;
-    technologies: string[],
-    link: string,
-  }
-  
-  export const projects: Project[] = [
-    {
-      slug: 'proyecto-1',
-      title: 'Proyecto 1',
-      description: 'Descripción breve del Proyecto 1',
-      imageUrl: './../../public/assets/astro.svg',
-      details: 'Detalles completos del Proyecto 1...',
-      technologies: ['TypeScript', 'Astro'],
-      link: 'https://project-one-link.com',
-    },
-    {
-      slug: 'proyecto-2',
-      title: 'Proyecto 2',
-      description: 'Descripción breve del Proyecto 2',
-      imageUrl: './../../public/assets/astro.svg',
-      details: 'Detalles completos del Proyecto 2...',
-      technologies: ['TypeScript', 'Astro'],
-      link: 'https://project-one-link.com',
-    },
-    {
-      slug: 'proyecto-3',
-      title: 'Proyecto 3',
-      description: 'Descripción breve del Proyecto 3',
-      imageUrl: './../../public/assets/astro.svg',
-      details: 'Detalles completos del Proyecto 3...',
-      technologies: ['TypeScript', 'Astro'],
-      link: 'https://project-one-link.com',
-    },
-  ];
-  
+  slug: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  details: string;
+  technologies: string[];
+  link: string;
+  androidLink?: string;
+  iosLink?: string;
+}
+
+export const projects: Project[] = [
+  {
+    slug: "proyecto-1",
+    title: "Storiesi",
+    description:
+      "Participe en el desarrollo de una api rest para una una aplicación móvil de red social para escritores",
+    imageUrl: "./../../public/assets/storiesi.jpg",
+    details: "Detalles completos del Proyecto 1...",
+    technologies: ["Postgresql", "Nestjs"],
+    link: "",
+  },
+  {
+    slug: "proyecto-2",
+    title: "Scouting",
+    description:
+      "Participé en el desarollo de una aplicación móvil para la reserva de lugares para que artistas lleven a cabo diferentes tipos de eventos",
+    imageUrl: "./../../public/assets/scouting.jpg",
+    details: "Detalles completos del Proyecto 2...",
+    technologies: ["React Native", "Firebase"],
+    link: "",
+    androidLink: "https://play.google.com/store/apps/details?id=com.distsoftware.scoutingapp&pcampaignid=web_share",
+    iosLink: "https://apps.apple.com/co/app/scouting/id1589683957"
+  },
+  {
+    slug: "proyecto-3",
+    title: "Quicook",
+    description: "Proyecto desarrollado de la mano de No Country y un gran grupo de trabajo, en el que desarrollé una api con Expressjs, MongoDB, usando passport y manejo de roles",
+    imageUrl: "./../../public/assets/quicook.jpeg",
+    details: "Detalles completos del Proyecto 3...",
+    technologies: ["Express", "MongoDB"],
+    link: "https://nocountry-frontend.onrender.com/",
+  },
+];
